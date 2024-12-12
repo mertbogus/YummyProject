4c2996056b552f9bbdc807a544b0c1cd1c1474fb
mertbogus <bogusmert@gmail.com>
2024-12-12 22:28:27 +0300

    first commit

---
+++ Models/ChefSocial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YummyProject.Models
+{
+    public class ChefSocial
+    {
+        public int ChefSocialId { get; set; }
+        public string SocialUrl { get; set; }
+        public string Icon { get; set; }
+        public string Name { get; set; }
+
+        public int ChefId { get; set; }
+        public virtual Chef Chef { get; set; }
+
+        
+    }
+}
